7192c47c377bb87215e64746d487013654402a8a
Vaibhav <vkalway@gmail.com>
2025-11-19 16:14:48 +0530

    Updated icon color for on/off states

--- background.js
+++ background.js
@@ -4,7 +4,30 @@ chrome.runtime.onInstalled.addListener(() => {
     if (typeof res.enabled === 'undefined') {
       chrome.storage.local.set({enabled: true});
     }
+    // Set initial icon based on state
+    updateIcon(res.enabled !== false);
   });
+});
+
+// Update icon based on enabled state
+function updateIcon(enabled) {
+  const iconSet = enabled ? {
+    "16": "icons/icon16.png",
+    "48": "icons/icon48.png",
+    "128": "icons/icon128.png"
+  } : {
+    "16": "icons/icon16-disabled.png",
+    "48": "icons/icon48-disabled.png",
+    "128": "icons/icon128-disabled.png"
+  };
+  chrome.action.setIcon({ path: iconSet });
+}
+
+// Listen for storage changes to update icon
+chrome.storage.onChanged.addListener((changes, area) => {
+  if (area === 'local' && changes.enabled) {
+    updateIcon(changes.enabled.newValue);
+  }
 });
 
 // also forward action from popup (optional)
